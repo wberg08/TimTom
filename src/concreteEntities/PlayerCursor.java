b841712a561ea4bc30c3141405469f5d01a8ac96
William Berg <william.berg08@gmail.com>
2013-04-16 22:20:12 +0100

    fix hitbox behaviour of test PlayerCursor

--- src/concreteEntities/PlayerCursor.java
+++ src/concreteEntities/PlayerCursor.java
@@ -1,6 +1,5 @@
 package concreteEntities;
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.HashSet;
@@ -13,53 +12,49 @@ import logic.entities.shapedefPrimitives.Shapedef;
 import system.InputProvider;
 
 public class PlayerCursor extends PhysicsActor {
-  
+
   public PlayerCursor(int xLoc, int yLoc) {
     super();
     this.xLoc = xLoc;
     this.yLoc = yLoc;
-    
+
     Set<Shapedef> hitbox = new HashSet<Shapedef>();
     hitbox.add(new Circle(xLoc, yLoc, 5));
     setHitbox(hitbox);
   }
 
   public void step(GameFrame currentFrame) {
-    if(InputProvider.isUpPressed) {
+    setHitboxX(xLoc);
+    setHitboxY(yLoc);
+    
+    if(InputProvider.isUpPressed)
       yLoc -= 1;
-      moveHitboxy(-1);
-    }
-    if(InputProvider.isDownPressed) {
+    if(InputProvider.isDownPressed)
       yLoc += 1;
-      moveHitboxy(1);
-    }
-    if(InputProvider.isLeftPressed) {
+    if(InputProvider.isLeftPressed)
       xSpd -= 1;
-      moveHitboxx(-1);
-    }
-    if(InputProvider.isRightPressed) {
+    if(InputProvider.isRightPressed)
       xSpd += 1;
-      moveHitboxx(1);
-    }
   }
 
   @Override
   public void paint(Graphics g) {
     g.setColor(Color.BLACK);
-    g.fillOval(xLoc-5, yLoc-5, 10, 10);
+    g.fillOval(xLoc - 5, yLoc - 5, 10, 10);
   }
-  
-  private void moveHitboxx(int d) {
-    for(Shapedef s : hitbox) {
+
+  private void setHitboxX(int x) {
+    for (Shapedef s : hitbox) {
       Circle c = (Circle) s;
-      c.xLoc += d;
+      c.xLoc = x;
     }
   }
-  
-  private void moveHitboxy(int d) {
-    for(Shapedef s : hitbox) {
+
+  private void setHitboxY(int y) {
+    for (Shapedef s : hitbox) {
       Circle c = (Circle) s;
-      c.yLoc += d;
+      c.yLoc = y;
     }
   }
+
 }
